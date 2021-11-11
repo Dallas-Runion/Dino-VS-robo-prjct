a9c501be9346d81608e86eaaa8614ec59baebed7
unknown <dallas.runion10@gmail.com>
2021-11-11 10:43:31 +0300

    added weapon class folder along with method with a pass for now

---
+++ weapon.py
@@ -0,0 +1,7 @@
+class Weapon:
+    name_weapon = "laser gun 9000"
+    attack_power = 10
+    def __init__(self, name, attack_power):
+        self.name = name
+        self.attack_power = attack_power
+        pass
