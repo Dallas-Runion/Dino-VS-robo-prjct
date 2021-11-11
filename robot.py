6947dc5171635aaf9ce0518ea309a8f6cf670f47
unknown <dallas.runion10@gmail.com>
2021-11-11 10:51:55 +0300

    Added robot class folder and a pass foe methods until complete

---
+++ robot.py
@@ -0,0 +1,10 @@
+from weapon import Weapon
+class Robot:
+    name = "Death_bot_z9"
+    health = 100
+    weapon = Weapon
+    def __init__(self, name_bot):
+        self.name = name_bot
+        pass
+    def attack(self, name, attack_power):
+        pass
