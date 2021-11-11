178edbc18467f2b413a1252c40ffc362d6091bfb
unknown <dallas.runion10@gmail.com>
2021-11-11 11:15:54 +0300

    Added battlefield class folder thus complete the shell of the task. All imports are placed where needed also

---
+++ battlefield.py
@@ -0,0 +1,21 @@
+from herd import Herd
+from fleet import Fleet
+class Battlefield:
+    def __init__(self):
+        pass
+    def run_game(self):
+        pass
+    def display_welcome(self):
+        pass
+    def battle(self):
+        pass
+    def dino_turn(self):
+        pass
+    def robo_turn(self):
+        pass
+    def show_dino_opponet_options(self):
+        pass
+    def show_robo_opponet_options(self):
+        pass
+    def display_winner(self):
+        pass
